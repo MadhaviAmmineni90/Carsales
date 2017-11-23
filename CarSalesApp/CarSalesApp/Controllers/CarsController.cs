a71eaf54c73aea9e2a8fa382eafbad9dfe1abe89
ProxyWebLtd <madhaviammineni@gmail.com>
2017-11-23 21:36:50 +1100

    Added API methods to Gets and Edit the vehicles

--- CarSalesApp/CarSalesApp/Controllers/CarsController.cs
+++ CarSalesApp/CarSalesApp/Controllers/CarsController.cs
@@ -9,16 +9,54 @@ namespace CarSalesApp.Controllers
 {
     public class CarsController : ApiController
     {
+        private CarSalesDBEntities objcarDbentities = new CarSalesDBEntities();
+        /// <summary>
+        /// Method to Get the list of vehicles
+        /// </summary>
+        /// <returns>List</returns>
         [HttpGet]
-        public IEnumerable<tblCar> Get()
+        public IEnumerable<tblCar> GetCars()
         {
-            using (CarSalesDBEntities entities = new CarSalesDBEntities())
-            {
-                entities.Configuration.ProxyCreationEnabled = false;
-                return entities.tblCars.ToList();
-            }
+            objcarDbentities.Configuration.ProxyCreationEnabled = false;
+            return objcarDbentities.tblCars.ToList();
+
         }
 
-      
+        /// <summary>
+        /// Method to Update existing Cars
+        /// </summary>
+        /// <param name="CarId"></param>
+        /// <param name="objCar"></param>
+        /// <returns>IHttpActionResult</returns>
+        [HttpPut]
+        public IHttpActionResult PutCars(int CarId, tblCar objCar)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Not a valid data");
+            }
+
+            if (CarId != objCar.CarId)
+            {
+                return BadRequest();
+            }
+            var existingCar = objcarDbentities.tblCars.Where(c => c.CarId == objCar.CarId).FirstOrDefault<tblCar>();
+            if (existingCar != null)
+            {
+                existingCar.Make = objCar.Make;
+                existingCar.Model = objCar.Model;
+                existingCar.Engine = objCar.Engine;
+                existingCar.Doors = objCar.Doors;
+                existingCar.wheels = objCar.wheels;
+                existingCar.cartype = objCar.cartype;
+                objcarDbentities.SaveChanges();
+            }
+            else
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+
     }
 }
