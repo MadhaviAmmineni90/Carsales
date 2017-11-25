5a86379e451585b6c2523b437ecb8284dd425f73
ProxyWebLtd <madhaviammineni@gmail.com>
2017-11-26 10:22:51 +1100

    Vehicles Create component Added

---
+++ CarSalesApp/CarSalesAngularWeb/src/app/Vehicles/VehicleCreate.component.ts
@@ -0,0 +1,33 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { ICars } from './Vehicles';
+import { VehicleService } from './Vehicles.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+    selector: 'list-cars',
+    templateUrl: 'app/Vehicles/VehicleCreate.component.html',
+    providers: [VehicleService]
+})
+
+    ///Cars Component
+export class carsCreateComponent {
+    cars: ICars;
+
+
+    AddCarsForm: FormGroup;
+    title: string = "AddCar";
+    constructor(private _fb: FormBuilder) {
+        this.AddCarsForm = this._fb.group({
+            Make: '',
+            Model: ['', [Validators.required]],
+            Engine: '',
+            Doors: ['', [Validators.required]],
+            Wheels: ['', [Validators.pattern("[1-9][0-9]{9}")]],
+            CarType: '',
+        })
+    }
+    ngOnInit() {
+
+    }
+}
