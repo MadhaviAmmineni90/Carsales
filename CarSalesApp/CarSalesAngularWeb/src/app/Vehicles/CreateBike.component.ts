bc779ea20c672de5ff48d18591c14ff006fe6b89
ProxyWebLtd <madhaviammineni@gmail.com>
2017-11-26 21:19:23 +1100

    New Components added in type script file ,added to create new bikes New html forms created to Get the list of bikes and add new bike

---
+++ CarSalesApp/CarSalesAngularWeb/src/app/Vehicles/CreateBike.component.ts
@@ -0,0 +1,32 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { IBikes } from './Vehicles';
+import { VehicleService } from './Vehicles.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+    selector: 'list-cars',
+    templateUrl: 'app/Vehicles/VehicleCreate.component.html',
+    providers: [VehicleService]
+})
+
+    ///BikesComponent
+export class bikesCreateComponent {
+    bikes: IBikes;
+
+    AddBikesForm: FormGroup;
+    title: string = "AddBike";
+    constructor(private _fb: FormBuilder) {
+        this.AddBikesForm = this._fb.group({
+            Make: '',
+            Model: ['', [Validators.required]],
+            Engine: '',
+            Wheels: ['', [Validators.pattern("[1-9][0-9]{9}")]],
+            BikeType: '',
+        })
+    }
+
+    ngOnInit() {
+
+    }
+}
